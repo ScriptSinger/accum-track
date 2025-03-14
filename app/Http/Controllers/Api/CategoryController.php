6bced0915143b4cad3511aa4044763326dd30809
ScriptSinger <ufamasters102@gmail.com>
2025-03-15 00:15:46 +0500

    Implemented CRUD API for categories

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categories = Category::all();
+
+        return response()->json($categories, Response::HTTP_OK);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string|max:255|unique:categories,name',
+        ]);
+
+        $category = Category::create($data);
+
+        Log::info('Создание категории', $data);
+
+        return response()->json($category, Response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $category = Category::find($id);
+
+        if (!$category) {
+            return response()->json([
+                'message' => 'Категория не найдена'
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        return response()->json($category, Response::HTTP_OK);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $category = Category::find($id);
+
+        if (!$category) {
+            return response()->json([
+                'message' => 'Категория не найдена'
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        $data = $request->validate([
+            'name' => 'required|string|max:255|unique:categories,name,' . $category->id,
+        ]);
+
+        $category->update($data);
+
+        return response()->json($category, Response::HTTP_OK);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $category = Category::find($id);
+
+        if (!$category) {
+            return response()->json([
+                'message' => 'Категория не найдена'
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        $category->delete();
+
+        return response()->json([
+            'message' => 'Категория успешно удалена'
+        ], Response::HTTP_OK);
+    }
+}
